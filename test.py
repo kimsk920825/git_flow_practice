ef215516e295678f797e76c89cdec680ad2d33c5
kimsk920825 <kimseungkyubestiedu@gmail.com>
2020-12-22 18:58:08 +0900

    excel_function_added

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 import time
-
+import openpyxl
 def now():
     time()
+
+def write_excel_template(filename, sheetname, datalist):
+    excel_file = openpyxl.Workbook
+    excel_sheet = excel_file.active
+    excel_sheet.column_dimensions['A'].width =100 
